ff4677100de08ef635dcf7f81b6f373e542494c2
haruuuu-gi <lovarazakasoa@gmail.com>
2026-07-20 12:09:53 +0300

    Filters

---
+++ 4336-4294/app/Filters/ClientAuthFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ClientAuthFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        $session = session();
+
+        if (! $session->get('compte_id')) {
+            return redirect()->to('/client/login')->with('error', 'Veuillez vous connecter.');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // rien à faire
+    }
+}
